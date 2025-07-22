0538184d207f7c42b8d68cae8aa3bc3788a2e6dd
Nouraldeen Kanaan <nourkn3ann@gmail.com>
2025-07-22 15:38:00 +0300

    Convert login from txt file to Registry

--- DVLD/GlobalClasses/clsGlobal.cs
+++ DVLD/GlobalClasses/clsGlobal.cs
@@ -1,48 +1,28 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
+using Microsoft.Win32;
 using System.IO;
 
 namespace DVLD_Business
 {
     public static class clsGlobal
     {
-        private static string FilePath = "E:\\Git\\DVLD\\Login\\UsersInfo.txt";
+        private static string LoginInfoPath = @"HKEY_CURRENT_USER\SOFTWARE\LoginInfo";
 
         public static clsUser CurrentUser;
 
         public static bool RememberUsernameAndPassword(string username, string password)
         {
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-            {
-                File.WriteAllText(FilePath,string.Empty);
-                return false;
-            }
-            else
-                File.WriteAllText(FilePath,$"{username}|{password}");
+            Registry.SetValue(LoginInfoPath, "Username", username, RegistryValueKind.String);
+            Registry.SetValue(LoginInfoPath, "Password", password, RegistryValueKind.String);
 
             return true;
         }
         public static bool LoadCredential(ref string Username,ref string Password)
         {
-            if (File.Exists(FilePath))
-            {
-                string FullText = File.ReadAllText(FilePath);
+            Username = Registry.GetValue(LoginInfoPath, "Username", null) as string;
+            Password = Registry.GetValue(LoginInfoPath, "Password", null) as string;
 
-                if (FullText.Length > 1)
-                {
-                    string[] Data = FullText.Split('|');
-                    Username = Data[0];
-                    Password = Data[1];
-                    return true;
-                }
-                else
-                {
-                    Username = "";
-                    Password = "";
-                }
-            }
-            return false;
+            return true;
         }
     }
 }
